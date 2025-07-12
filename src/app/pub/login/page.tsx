82229ecf8d5a1790af7d220f3f03ccc7c2ad02b9
yunny <suk921@gmail.com>
2025-07-12 17:02:21 +0900

    edit: image size 추가

--- src/app/pub/login/page.tsx
+++ src/app/pub/login/page.tsx
@@ -22,7 +22,7 @@ export default function LoginPage() {
     return (
         <div className={styles.wrapper}>
             <header className={styles.header}>
-                <Image src="/images/byzip_logo.png" alt="분양모음집 로고" loading="eager" priority/>
+                <Image src="/images/byzip_logo.png" alt="분양모음집 로고" width={30} height={45}  loading="eager" priority/>
                 <span>분양모음집-관리자</span>
             </header>
 
